1abf1dc3d679fbc3734faf03b223f124a39588b9
daniel.ribeiroc <daniel.ribeiroc@edu.hefr.ch>
2023-10-22 20:15:25 +0200

    ADD : v1 --> principal version + requirements update

--- DashF1_Comparaison.py
+++ DashF1_Comparaison.py
@@ -4,9 +4,8 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
 import time
-
 from dash import dcc, html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import fastf1 as ff1
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
@@ -18,10 +17,17 @@ import matplotlib                                # pip install matplotlib
 matplotlib.use('agg')
 import base64
 from io import BytesIO
+from dash.long_callback import DiskcacheLongCallbackManager
+import dash._callback_context as ctx
+## Diskcache
+import diskcache
 
+cache = diskcache.Cache("./cache")
+long_callback_manager = DiskcacheLongCallbackManager(cache)
 session = None
+tab_test = None
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], long_callback_manager=long_callback_manager)
 
 app.layout = dbc.Container([
     html.H1("Comparaison", className='mb-2', style={'textAlign': 'center'}),
@@ -29,8 +35,26 @@ app.layout = dbc.Container([
         dcc.Tab(label='Comparaison entre pilotes', value='tab-1', children=[
             html.Div([
                 html.Br(),
-                html.H3('Welcome to Tab 2!', style={'margin-top': '10px'}),
+                html.H3('Vue comparaison entre 2 pilotes', style={'margin-top': '10px'}),
+                html.P("Dans cet onglet, les utilisateurs ont la possibilité de comparer les performances de deux pilotes lors d'une course spécifique. n cas d'erreur ou de problème lors du "
+                       "chargement des données ou du traitement des informations, un message d'erreur explicite est affiché pour informer l'utilisateur de la situation, garantissant ainsi une expérience utilisateur transparente et informative.", style={'margin-top': '10px'}),
                 html.Br(),
+                dbc.Alert(
+                    "La session n'a pas de données (potentiellement qu'elle ne s'est pas encore déroulé)",
+                    id="alert-no-fade",
+                    dismissable=True,
+                    fade=False,
+                    is_open=False,
+                    color="danger"
+                ),
+                dbc.Alert(
+                    "Hello! I am an alert that doesn't fade in or out",
+                    id="alert-no-fade2",
+                    dismissable=True,
+                    fade=False,
+                    is_open=False,
+                    color="danger"
+                ),
                 dbc.Row([
                     dbc.Col([
                         dcc.Dropdown(
@@ -66,6 +90,19 @@ app.layout = dbc.Container([
                         )
                     ])
                 ])
+            ]),
+            dbc.Row([
+                dbc.Col([html.Button(id="button_id", children="Comparer les 2 pilotes !")]),
+                dbc.Col([html.Button(id="cancel_button_id", children="Interrompre le processus")])
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    dcc.Loading(
+                        id="loading-bar_graph",
+                        children=[html.Img(id='bar-graph-matplotlib')],
+                        type="circle",
+                    )
+                ], width=12)
             ])
         ]),
         dcc.Tab(label='Tab 2', value='tab-2', children=[
@@ -75,14 +112,6 @@ app.layout = dbc.Container([
                     ])
         ]),
     ]),
-    dbc.Row([
-        html.Button('Specific Button', id='specific-button'),
-    ]),
-    dbc.Row([
-        dbc.Col([
-            html.Img(id='bar-graph-matplotlib')
-        ], width=12)
-    ])
 ])
 def time_consuming_function(selected_year, selected_month):
     # Simulate a time-consuming operation
@@ -101,35 +130,48 @@ def update_dropdown_2(selected_year):
     print("------------------------------------------------------")
     print(selected_year)
     print("------------------------------------------------------")
-    session = ff1.get_event_schedule(selected_year)['Country']
+    session = ff1.get_event_schedule(selected_year)['EventName']
     #session = ff1.get_event_schedule(2023)['Country']
     tab_countries = [i for i in session]
+    #[{'label': list[i], 'value': i} for i in range(len(list))]
     return [dcc.Dropdown(id='dropdown-2', options=tab_countries, className='mb-3')], tab_countries, tab_countries[0]
 
 @app.callback(
     Output('loading-dropdown-3', 'children'),
     Output('dropdown-3', 'options'),
     Output('dropdown-3', 'value'),
+    Output("alert-no-fade", "is_open"),
     Input('dropdown-1', 'value'),
     Input('dropdown-2', 'value')
 )
-def update_dropdown_3(selected_year, selected_month):
+def update_dropdown_3(selected_year, selected_race):
     global session
-    if selected_month is None:
+    global tab_test
+    if selected_race is None:
         raise PreventUpdate
 
-    year_schedule = ff1.get_event_schedule(selected_year)['Country']
-    indices = [index for index, country in enumerate(year_schedule) if country == selected_month]
-    session = ff1.get_session(selected_year, year_schedule[indices[0]], "R")
-    session.load(telemetry=False, laps=False, weather=False)
-    drivers = [i for i in session.drivers]
-    drivers_name = [session.get_driver(i)["Abbreviation"] for i in drivers]
-    return [dcc.Dropdown(id='dropdown-3', options=drivers_name, className='mb-3')], drivers_name, drivers_name[0]
+    year_schedule = ff1.get_event_schedule(selected_year)['EventName']
+    indices = [index for index, country in enumerate(year_schedule) if country == selected_race]
+    print("Print indices :")
+    print(indices)
+    if indices==None:
+        indices=0
+
+    try:
+        session = ff1.get_session(selected_year, year_schedule[indices[0]], "R")
+        session.load(telemetry=False, laps=False, weather=False)
+        drivers = [i for i in session.drivers]
+        drivers_name = [session.get_driver(i)["Abbreviation"] for i in drivers]
+        return [dcc.Dropdown(id='dropdown-3', options=drivers_name, className='mb-3')], drivers_name, drivers_name[0], False
+    except:
+        return [dcc.Dropdown(id='dropdown-3', options=["None"], className='mb-3')], ["None"], ["None"], True
+
 
 @app.callback(
     Output('loading-dropdown-4', 'children'),
     Output('dropdown-4', 'options'),
     Output('dropdown-4', 'value'),
+    Output("alert-no-fade2", "is_open"),
     Input('dropdown-1', 'value'),
     Input('dropdown-2', 'value'),
     Input('dropdown-3', 'value')
@@ -138,114 +180,164 @@ def update_dropdown_4(selected_year, selected_month, selected_day):
     global session
     if selected_day is None:
         raise PreventUpdate
-    session.load(telemetry=False, laps=False, weather=False)
-    drivers = [i for i in session.drivers]
-    drivers_name = [session.get_driver(i)["Abbreviation"] for i in drivers]
-    return [dcc.Dropdown(id='dropdown-4', options=drivers_name, className='mb-3')], drivers_name, drivers_name[1]
-@app.callback(
-    Output(component_id='bar-graph-matplotlib', component_property="src"),
-    Input('dropdown-1', 'value'),
-    Input('dropdown-2', 'value'),
-    Input('dropdown-3', 'value'),
-    Input('dropdown-4', 'value')
-)
-def plot_data(year, race, driver1, driver2):
-    #if n_clicks is None:
-    #    raise PreventUpdate
+    try:
+        session.load(telemetry=False, laps=False, weather=False)
+        drivers = [i for i in session.drivers]
+        drivers_name = [session.get_driver(i)["Abbreviation"] for i in drivers]
+        return [dcc.Dropdown(id='dropdown-4', options=drivers_name, className='mb-3')], drivers_name, drivers_name[1], False
+    except:
+        return [dcc.Dropdown(id='dropdown-4', options=["None"], className='mb-3')], ["None"], ["None"], False
 
-        # Disable the button
-    ctx = dash.callback_context
-    button_id = ctx.triggered_id
-    button_prop_name = 'disabled'
-    button_state = [{button_id: True, button_prop_name: True}]
+@app.long_callback(
+    output=Output(component_id='bar-graph-matplotlib', component_property="src"),
+    inputs=[Input("button_id", "n_clicks")],
+    state=[State('dropdown-1', 'value'),
+           State('dropdown-2', 'value'),
+           State('dropdown-3', 'value'),
+           State('dropdown-4', 'value')],
+    running=[
+        (Output("button_id", "disabled"), True, False),
+        (Output("cancel_button_id", "disabled"), False, True),
+    ],
+    cancel=[Input("cancel_button_id", "n_clicks")],
+)
+def plot_data(n_clicks, year, race, driver1, driver2):
+    #n_clicks, year, race, driver1, driver2
     print("PLOT DATA")
     fig_bar_matplotlib = build_comparaison_tab(year, race, driver1, driver2)
-    print("FIG BAR - Done")
+    print("Done")
     return fig_bar_matplotlib
 
 def build_comparaison_tab(year, race, driver1, driver2):
     global session
-    year_schedule = ff1.get_event_schedule(year)['Country']
-    indices = [index for index, country in enumerate(year_schedule) if country == race]
-    _race = year_schedule[indices[0]]
-    session = ff1.get_session(year, _race, "R")
-    session.load(telemetry=True, laps=True, weather=False)
-    lap_driver1 = session.laps.pick_driver(driver1).pick_fastest()
-    lap_driver2 = session.laps.pick_driver(driver2).pick_fastest()
-    # Calculate lap times for both drivers
-    lap = lap_driver1
-    lap_time_driver1 = lap_driver1.telemetry['Time'].values
-    lap_time_driver2 = lap_driver2.telemetry['Time'].values
-    who_fastest = []
-    # Jump 10 laps at a time and compare lap times
-    for i in range(0, len(lap_time_driver1) - 10, 10):
-        lap_time_1_chunk = lap_time_driver1[i + 10] - lap_time_driver1[i]
-        lap_time_2_chunk = lap_time_driver2[i + 10] - lap_time_driver2[i]
+    global tab_test
+    print(tab_test)
+    try:
 
-        if lap_time_1_chunk < lap_time_2_chunk:
-            for j in range(10):
-                who_fastest.append(True)
+        print(f"BEFORE : Values selected : {year} - {race} - {driver1} - {driver2}")
+
+        if race == None:
+            _1race = "Bahrain Grand Prix"
         else:
-            for j in range(10):
-                who_fastest.append(False)
+            _1race = race
+        if driver1 == None:
+            _driver1 = "VER"
+        else:
+            _driver1 = driver1
+        if driver2 == None:
+            _driver2 = "PER"
+        else:
+            _driver2 = driver2
 
-    # Check if there are any remaining laps that are not a multiple of 10
-    remaining_laps = len(lap_time_driver1) % 10
-    if remaining_laps > 0:
-        # Compare the remaining laps individually
-        for i in range(len(lap_time_driver1) - remaining_laps, len(lap_time_driver1)):
-            if lap_time_driver1[i] < lap_time_driver2[i]:
-                who_fastest.append(True)
+        print(f"AFTER : Values selected : {year} - {_1race} - {_driver1} - {_driver2}")
+        year_schedule = ff1.get_event_schedule(year)['EventName']
+        indices = [index for index, country in enumerate(year_schedule) if country == _1race]
+        _race = year_schedule[indices[0]]
+        session = ff1.get_session(year, _1race, "R")
+        session.load(telemetry=True, laps=True, weather=False)
+        lap_driver1 = session.laps.pick_driver(_driver1).pick_fastest()
+        lap_driver2 = session.laps.pick_driver(_driver2).pick_fastest()
+        # Calculate lap times for both drivers
+        lap = lap_driver1
+        lap_time_driver1 = lap_driver1.telemetry['Time'].values
+        lap_time_driver2 = lap_driver2.telemetry['Time'].values
+        who_fastest = []
+
+        if len(lap_time_driver1) < len(lap_time_driver2):
+            # Make lap_time_driver1 the same length as lap_time_driver2
+            lap_time_driver2 = lap_time_driver2[:len(lap_time_driver1)]
+        elif len(lap_time_driver2) < len(lap_time_driver1):
+            # Make lap_time_driver2 the same length as lap_time_driver1
+            lap_time_driver1 = lap_time_driver1[:len(lap_time_driver2)]
+        else:
+            # Both arrays are already of the same length, no modification needed
+            pass
+
+        print(f"Length of the driver {driver1} : {len(lap_time_driver1)}")
+        print(f"Length of the driver {driver2} : {len(lap_time_driver2)}")
+        # Jump 10 laps at a time and compare lap times
+        for i in range(0, len(lap_time_driver1) - 10, 10):
+            lap_time_1_chunk = lap_time_driver1[i + 10] - lap_time_driver1[i]
+            lap_time_2_chunk = lap_time_driver2[i + 10] - lap_time_driver2[i]
+
+            if lap_time_1_chunk < lap_time_2_chunk:
+                for j in range(10):
+                    who_fastest.append(True)
             else:
-                who_fastest.append(False)
+                for j in range(10):
+                    who_fastest.append(False)
 
+        # Check if there are any remaining laps that are not a multiple of 10
+        remaining_laps = len(lap_time_driver1) % 10
+        if remaining_laps > 0:
+            # Compare the remaining laps individually
+            for i in range(len(lap_time_driver1) - remaining_laps, len(lap_time_driver1)):
+                if lap_time_driver1[i] < lap_time_driver2[i]:
+                    who_fastest.append(True)
+                else:
+                    who_fastest.append(False)
     # Get telemetry data
-    x = lap.telemetry['X']  # values for x-axis
-    y = lap.telemetry['Y']  # values for y-axis
+        x = lap.telemetry['X']  # values for x-axis
+        y = lap.telemetry['Y']  # values for y-axis
 
-    points = np.array([x, y]).T.reshape(-1, 1, 2)
-    segments = np.concatenate([points[:-1], points[1:]], axis=1)
-    # We create a plot with title and adjust some setting to make it look good.
-    fig, ax = plt.subplots(sharex=True, sharey=True, figsize=(12, 6.75))
-    fig.suptitle(f'{race} - {year} - {driver1} vs {driver2} - Speed', size=24, y=0.97)
+        points = np.array([x, y]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+        # We create a plot with title and adjust some setting to make it look good.
+        fig, ax = plt.subplots(sharex=True, sharey=True, figsize=(12, 6.75))
+        fig.suptitle(f'{_1race} - {year} - {_driver1} vs {_driver2} - Speed', size=24, y=0.97)
 
-    # Adjust margins and turn of axis
-    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.12)
-    ax.axis('off')
+        # Adjust margins and turn of axis
+        plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.12)
+        ax.axis('off')
 
-    mask = np.array(who_fastest)  # Repeat each element 10 times for each sector
+        mask = np.array(who_fastest)  # Repeat each element 10 times for each sector
 
-    # After this, we plot the data itself.
-    # Create background track line
-    ax.plot(lap.telemetry['X'], lap.telemetry['Y'], color='black', linestyle='-', linewidth=16, zorder=0)
-    norm = plt.Normalize(0, 1)
-    # Set the color based on the mask
-    lc = LineCollection(segments, cmap=mpl.colors.ListedColormap([fastf1.plotting.driver_color(driver1), fastf1.plotting.driver_color(driver2)]), norm=norm, linewidth=5)
-    lc.set_array(mask)
+        # After this, we plot the data itself.
+        # Create background track line
+        ax.plot(lap.telemetry['X'], lap.telemetry['Y'], color='black', linestyle='-', linewidth=16, zorder=0)
+        norm = plt.Normalize(0, 1)
+        # Set the color based on the mask
+        lc = LineCollection(segments, cmap=mpl.colors.ListedColormap([fastf1.plotting.driver_color(_driver1), fastf1.plotting.driver_color(_driver2)]), norm=norm, linewidth=5)
+        lc.set_array(mask)
 
-    # Set the values used for colormapping
-    lc.set_array(mask)
+        # Set the values used for colormapping
+        lc.set_array(mask)
 
-    # Merge all line segments together
-    line = ax.add_collection(lc)
+        # Merge all line segments together
+        line = ax.add_collection(lc)
 
-    # Finally, we create a color bar as a legend.
-    # Create a color bar as a legend.
-    cbaxes = fig.add_axes([0.25, 0.05, 0.5, 0.05])
-    normlegend = mpl.colors.Normalize(vmin=0, vmax=1)  # Assuming 'who_fastest' contains True and False values
-    legend_labels = [f"Driver {driver1} Fastest", f"Driver {driver2} Fastest"]
-    legend = mpl.colorbar.ColorbarBase(cbaxes, norm=normlegend, cmap=mpl.colors.ListedColormap([fastf1.plotting.driver_color(driver1), fastf1.plotting.driver_color(driver2)]),
-                                       orientation="horizontal", ticks=[0, 1])
-    legend.set_ticklabels(legend_labels)
+        # Finally, we create a color bar as a legend.
+        # Create a color bar as a legend.
+        cbaxes = fig.add_axes([0.25, 0.05, 0.5, 0.05])
+        normlegend = mpl.colors.Normalize(vmin=0, vmax=1)  # Assuming 'who_fastest' contains True and False values
+        legend_labels = [f"Driver {_driver1} Fastest", f"Driver {_driver2} Fastest"]
+        legend = mpl.colorbar.ColorbarBase(cbaxes, norm=normlegend, cmap=mpl.colors.ListedColormap([fastf1.plotting.driver_color(_driver1), fastf1.plotting.driver_color(_driver2)]),
+                                           orientation="horizontal", ticks=[0, 1])
+        legend.set_ticklabels(legend_labels)
+        # Save it to a temporary buffer.
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+        # Embed the result in the html output.
+        fig_data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        fig_bar_matplotlib = f'data:image/png;base64,{fig_data}'
 
-    # Save it to a temporary buffer.
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    # Embed the result in the html output.
-    fig_data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    fig_bar_matplotlib = f'data:image/png;base64,{fig_data}'
+        return fig_bar_matplotlib
 
-    return fig_bar_matplotlib
+    except Exception as e:
+        # Handle the error here
+        print(f"Error occurred: {e}")
+        # Create an image with the error message
+        buf = BytesIO()
+        fig = plt.figure()
+        plt.text(0.5, 0.5, f"Error: {e}", ha='center', va='center', fontsize=10)
+        plt.axis('off')  # Turn off axis for the error message
+        fig.savefig(buf, format="png")
+        # Embed the result in the html output.
+        fig_data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        fig_bar_matplotlib = f'data:image/png;base64,{fig_data}'
+
+        return fig_bar_matplotlib
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8082)
